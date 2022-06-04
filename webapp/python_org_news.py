262565a350eee3835fe450f322d4b3e8b7e6bb3c
Artem Samoilov <samoylovartem07@gmail.com>
2022-06-04 23:37:36 +0800

    Refactoring, data bases support, some minor changes

--- webapp/python_org_news.py
+++ webapp/python_org_news.py
@@ -1,5 +1,7 @@
+from datetime import datetime
 import requests
 from bs4 import BeautifulSoup
+from webapp.model import db, News
 
 
 def get_html(url):
@@ -16,15 +18,20 @@ def get_python_news():
     if html:
         soup = BeautifulSoup(html, 'html.parser')
         all_news = soup.find('ul', class_='list-recent-posts').find_all('li')
-        result_news = []
         for news in all_news:
             title = news.find('a').text
             url = news.find('a')['href']
             published_date = news.find('time').text
-            result_news.append({
-                'title': title,
-                'url': url,
-                "published_date": published_date,
-            })
-        return result_news
-    return False
+            try:
+                published_date = datetime.strptime(published_date, '%b %d, %Y')
+            except ValueError:
+                published_date = datetime.now()
+            save_news(title, url, published_date)
+
+
+def save_news(title, url, published_date):
+    news_exists = News.query.filter(News.url == url).count()
+    if not news_exists:
+        news = News(title=title, url=url, published_date=published_date)
+        db.session.add(news)
+        db.session.commit()
